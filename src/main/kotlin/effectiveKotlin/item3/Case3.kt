414c0ac2160dc8229c58df9d6aced568b1d731b4
99winnmin <ojysep9987@gmail.com>
2024-01-25 02:33:48 +0900

    :bug: Fix : 충돌 문제 이슈로 인터페이스명 변경

--- src/main/kotlin/effectiveKotlin/item3/Case3.kt
+++ src/main/kotlin/effectiveKotlin/item3/Case3.kt
@@ -1,17 +1,17 @@
 package effectiveKotlin.item3
 
-interface UserRepo {
+interface UserRepoIF {
     fun getUserName() = JavaClass().value
 }
 
-class RepoImpl: UserRepo {
+class RepoImpl: UserRepoIF {
     override fun getUserName(): String? {
         return null
     }
 }
 
 fun main() {
-    val repo: UserRepo = RepoImpl()
+    val repo: UserRepoIF = RepoImpl()
     val text: String = repo.getUserName() // 런타임 시 NPE
     println("user name is ${text}")
 }
